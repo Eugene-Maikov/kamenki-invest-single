0836b81d1c0233a12b6b0edcc477a9239d764cd3
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-08-11 16:12:45 +0300

    feat: add tablet, mobile

--- assets/js/single-project.js
+++ assets/js/single-project.js
@@ -3,10 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const galleryVillageSwiper = new Swiper('.gallery-village__swiper', {
     slidesPerView: 4,
-    spaceBetween: 20,
+    spaceBetween: 30,
     navigation: {
       nextEl: '.gallery-village-next',
       prevEl: '.gallery-village-prev',
+    },
+    breakpoints: {
+      280: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
     },
   });
 
